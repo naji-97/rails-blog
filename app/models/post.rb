6280c9ca4d369af2306d513c03e4b26116cc5fc3
Naji-97 <elbairabennaji@gmail.com>
2023-07-12 14:32:13 +0100

    Impliement the reviwer suggestions

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,8 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  private
+
   def update_post_counter
     author.update(posts_counter: author.posts.count)
   end
@@ -10,4 +12,9 @@ class Post < ApplicationRecord
   def recent_comment(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
+
+  after_create :save
+  after_save :update_post_counter
+
+  after_destroy :update_post_counter
 end
