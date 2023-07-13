8780e22c2fceb47a836608fa0a043b69faf6ea98
Naji-97 <elbairabennaji@gmail.com>
2023-07-13 11:19:01 +0100

    add validations for user and post models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_create :save
+  after_save :update_comment_counter
+
+  after_destroy :update_comment_counter
+
   def update_comment_counter
     post.update(comments_counter: post.comments.count)
   end
--- app/models/like.rb
+++ app/models/like.rb
@@ -2,6 +2,9 @@ class Like < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post
 
+  after_create :save
+  after_save :update_like_counter
+
   def update_like_counter
     post.update(likes_counter: post.likes.count)
   end
