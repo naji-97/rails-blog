a28178fb84d32ef49fcb1310c9c60f62eae0e234
Naji-97 <elbairabennaji@gmail.com>
2023-07-17 16:28:31 +0100

    update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show] do
-      resources :comments, only: [:index, :create, :destroy]
     end
   end
 end
