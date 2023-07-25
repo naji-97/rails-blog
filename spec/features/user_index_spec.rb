ed413fc9d65ef55f2dac20c59262dd5dedd25456
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-25 08:42:24 +0000

    Set up user index page tests

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -1,33 +1,14 @@
 require 'rails_helper'
 RSpec.describe 'User Index Page', type: :feature do
-  let!(:user1) { User.create(name: 'User 1') }
-  let!(:user2) { User.create(name: 'User 2') }
-
-  it 'displays the username of all other users' do
-    visit users_path
-
-    expect(page).to have_content(user1.name)
-    expect(page).to have_content(user2.name)
+  before(:each) do
+    @user = User.create!(name: 'Example User', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 3,
+                           bio: 'I am a user')
+    @user2 = User.create!(name: 'Example User2', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 3,
+                          bio: 'I am a user')
+    @user3 = User.create!(name: 'Example User3', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 3,
+                          bio: 'I am a user')
+    @user4 = User.create!(name: 'Example User4', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 3,
+                          bio: 'I am a user')
   end
 
-  it 'displays the profile picture for each user' do
-    visit users_path
-
-    expect(page).to have_css("img[src='#{user1.photo}']")
-    expect(page).to have_css("img[src='#{user2.photo}']")
-  end
-
-  it 'displays the number of posts each user has written' do
-    visit users_path
-
-    expect(page).to have_content("Number of Posts: #{user1.posts_counter}")
-    expect(page).to have_content("Number of Posts: #{user2.posts_counter}")
-  end
-
-  it 'redirects to a user\'s show page when clicking on the user' do
-    visit users_path
-    click_link user1.name
-
-    expect(page).to have_current_path(user_path(user1))
-  end
 end
