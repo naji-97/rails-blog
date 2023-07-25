f0fcd621cda879310ced21b1d84d88096b058b8b
Naji-97 <elbairabennaji@gmail.com>
2023-07-25 19:24:08 +0100

    add style

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+    devise_for :users
+ # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
@@ -10,8 +11,8 @@ Rails.application.routes.draw do
 
   resources :users, only: [:index, :show] do
     resources :posts do
-      resources :comments, only: [:new, :create, :destroy]
-      resources :likes, only: [ :create]
+      resources :comments, only: [:new, :create]
+      resources :likes, only: [ :create, :new]
     end
   end
 end
