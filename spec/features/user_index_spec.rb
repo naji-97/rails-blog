bb078ff255fd144f9684dd38027298bba68bcc97
Harriet Oteng <harriet.oteng@gmail.com>
2023-07-25 08:44:12 +0000

    test for username and profile picture

--- spec/features/user_index_spec.rb
+++ spec/features/user_index_spec.rb
@@ -10,5 +10,17 @@ RSpec.describe 'User Index Page', type: :feature do
     @user4 = User.create!(name: 'Example User4', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', posts_counter: 3,
                           bio: 'I am a user')
   end
+  it 'displays the username of all other users' do
+    visit users_path
+    expect(page).to have_content 'List of Users'
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@user2.name)
+  end
 
+  it 'displays the profile picture for each user' do
+    visit users_path
+
+    expect(page).to have_css("img[src='#{@user.photo}']")
+    expect(page).to have_css("img[src='#{@user2.photo}']")
+  end
 end
