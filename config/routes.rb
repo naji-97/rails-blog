8bde60213cb4670be0fac8a159d9f4529d43ae1e
Naji-97 <elbairabennaji@gmail.com>
2023-07-26 11:51:47 +0100

    add confirmable to the user model

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@
 
 Rails.application.routes.draw do
 
-    devise_for :users
+    devise_for :users, controllers: {
+      confirmations: 'users/confirmations'
+    }
  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
